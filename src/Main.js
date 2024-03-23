59c2f9957c32fcab35df50ad779fd131158ace15
Merekeyev Dias <dikxarper.programming@gmail.com>
2024-03-23 22:04:30 +0500

    fixed post.js

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,13 @@
-
+import React from 'react';
 import Posts from './Posts';
+import data from './data'; // Import your data
 
-
-const Main = (props) => {
-    return(
-
-         <div className='container'>
-         
-            <Posts posts={props.data.posts}/>
+const Main = () => {
+    return (
+        <div className='container'>
+            <Posts posts={data.PostPage.posts}/> {/* Pass the 'posts' array from data */}
         </div>
-       
     )
 };
+
 export default Main;
--- src/Posts.js
+++ src/Posts.js
@@ -1,46 +1,55 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Post from "./Post";
+
 const Posts = (props) => {
-let postData = props.data.posts
-const [posts, setPosts] = useState(postData); const [text, setText] = useState('');
-const [postTitle, setTitle] = useState('');
-const addNewPost = () => {
-const newPost={
-id: Date.now(),
-text,
-postTitle
+    const [posts, setPosts] = useState(props.posts);
+    const [text, setText] = useState('');
+    const [postTitle, setTitle] = useState('');
+
+    const addNewPost = () => {
+        const newPost = {
+            id: Date.now(),
+            text,
+            postTitle
+        }
+        setPosts([...posts, newPost]);
+        setTitle('');
+        setText('');
+    }
+
+    return (
+        <div>
+            <form>
+                <div className="form-post">
+                    <div className="form-input"> 
+                        <input 
+                            value={postTitle}
+                            onChange={e => setTitle(e.target.value)} 
+                            type="text"
+                            placeholder="Название поста"
+                        />
+                    </div>
+                    <div className="form-input">
+                        <textarea 
+                            value={text}
+                            onChange={e => setText(e.target.value)} 
+                            type="text"
+                            placeholder="Описание поста" 
+                            rows="4" 
+                            cols="60" 
+                        />
+                    </div>
+                    <button type="button" onClick={addNewPost}>Oтправить</button>
+                </div>
+            </form>
+            <p className="posts-title">Posts:</p>
+            <div className="posts">
+                {posts.map(post => (
+                    <Post key={post.id} postTitle={post.postTitle} text={post.text} />
+                ))}
+            </div>
+        </div>
+    );
 }
-setPosts([...posts, newPost]);
-setTitle('');
-setText('');
 
-}
-return (
-   <div>
-<form>
-<div className="form-post">
-<div className="form-input"> <input value={postTitle}
-
-onChange={e => setTitle(e.target.value)} type="text"
-placeholder="Название поста"/>
-</div>
-<div className="form-input">
-<textarea value={text}
-
-onChange={e => setText(e.target.value)} type="text"
-placeholder="Описание поста" rows="4" cols="60" />
-</div>
-<button type="button" onClick={addNewPost}>Oтправить</button>
-</div>
-</form>
-<p className="posts-title">Posts:</p>
-      <div className="posts">
-        {posts.map(post => (
-          <Post key={post.id} postTitle={post.postTitle} text={post.text} />
-        ))}
-      </div>
-
-</div>
-);
-}
 export default Posts;
