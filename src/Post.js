9dfc19f87553600d65b1d62059ddb0caf5f29a45
Enver Yakubov <enveryakubov@gmail.com>
2024-03-24 10:04:30 +0300

    Добавила картинку которая отвечает за удаление поста

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,14 @@
 const Post = (props) => {
     return(
+    
         <div className="post">
         <h2>{props.postTitle}</h2>
         <p>{props.text}</p>
+        <img className="img-trash"  src="img/trash.png" alt="1"/>
         </div>
+     
+        
+     
     )
 }
 export default Post;
